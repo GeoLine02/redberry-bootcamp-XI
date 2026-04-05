2a23247f49a87ae6901d6e582d978a3b55e9a0ce
Nika Wuladze <126957977+GeoLine02@users.noreply.github.com>
2026-04-05 18:12:35 +0400

    feaT: create in progress courses list

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,14 +1,24 @@
+import { cookies } from "next/headers";
+import InCompleteCourses from "./components/InCompleteCourses";
 import Slider from "./components/Slider";
 import StartLerning from "./components/StartLerning";
 import { getFeaturedCourses } from "./services";
+import { getInprogressCourses } from "./services/index.server";
 
 export default async function Home() {
   const featuredCourses = await getFeaturedCourses();
+  const inProgressCourses = await getInprogressCourses();
 
+  const cookieStore = await cookies();
+  const token = cookieStore.get("accessToken")?.value;
+  console.log(token);
   return (
-    <div>
+    <div className="flex flex-col gap-8">
       <Slider />
-      <StartLerning courses={featuredCourses?.data ?? []} />
+      <div className={`${token ? "flex-col" : "flex-col-reverse"} flex gap-8`}>
+        <InCompleteCourses incompleteCourses={inProgressCourses?.data ?? []} />
+        <StartLerning courses={featuredCourses?.data ?? []} />
+      </div>
     </div>
   );
 }
